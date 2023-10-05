901240a127e37966254e6ac6f3907800e244aa18
Bathieuu <mathieu.broccoli@gmail.com>
2023-10-05 12:33:25 +0200

    HAAAAA

--- src/Controller/Admin/CityCrudController.php
+++ src/Controller/Admin/CityCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\City;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+
 
 class CityCrudController extends AbstractCrudController
 {
@@ -11,15 +15,12 @@ class CityCrudController extends AbstractCrudController
     {
         return City::class;
     }
-
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            TextField::new('zipcode'),
+            AssociationField::new('departement'),
         ];
     }
-    */
 }
