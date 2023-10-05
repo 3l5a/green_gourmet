fc00142b50b0bb625adebe1945cd3e172ae4b401
Elsa <elsa.thievet@hotmail.fr>
2023-10-05 12:32:04 +0200

    cards done

--- migrations/Version20231005074449.php
+++ migrations/Version20231005074449.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231004221559 extends AbstractMigration
+final class Version20231005074449 extends AbstractMigration
 {
     public function getDescription(): string
     {
